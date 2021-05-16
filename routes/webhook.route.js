27d37b338dad96fdd223863d78a0c2b08f356eb9
Nha NHV <nguyenhoangvannha@gmail.com>
2021-05-16 15:49:36 +0700

    Connect with facebook app to receive and reply to message and postback (#2)
    * Add Facebook webhook Verify_Token
    
    * Handle message and postbacks
    
    * Add missing lib

--- routes/webhook.route.js
+++ routes/webhook.route.js
@@ -1,6 +1,8 @@
 // https://developers.facebook.com/docs/messenger-platform/getting-started/webhook-setup
 
 const express = require('express');
+const request = require('request');
+const facebookConfig = require('../config/facebook.config.json');
 
 const router = express.Router();
 
@@ -19,12 +21,23 @@ router.post('/', (req, res) => {
   if (body.object === 'page') {
 
     // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function (entry) {
+    body.entry.forEach(function(entry) {
 
-      // Gets the message. entry.messaging is an array, but 
-      // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
+      // Gets the body of the webhook event
+      let webhookEvent = entry.messaging[0];
+      console.log(webhookEvent);
+
+      // Get the sender PSID
+      let senderPsid = webhookEvent.sender.id;
+      console.log('Sender PSID: ' + senderPsid);
+
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhookEvent.message) {
+        handleMessage(senderPsid, webhookEvent.message);
+      } else if (webhookEvent.postback) {
+        handlePostback(senderPsid, webhookEvent.postback);
+      }
     });
 
     // Returns a '200 OK' response to all requests
@@ -35,6 +48,98 @@ router.post('/', (req, res) => {
   }
 
 });
+
+// Handles messages events
+function handleMessage(senderPsid, receivedMessage) {
+  let response;
+
+  // Checks if the message contains text
+  if (receivedMessage.text) {
+    // Create the payload for a basic text message, which
+    // will be added to the body of your request to the Send API
+    response = {
+      'text': `You sent the message: '${receivedMessage.text}'. Now send me an attachment!`
+    };
+  } else if (receivedMessage.attachments) {
+
+    // Get the URL of the message attachment
+    let attachmentUrl = receivedMessage.attachments[0].payload.url;
+    response = {
+      'attachment': {
+        'type': 'template',
+        'payload': {
+          'template_type': 'generic',
+          'elements': [{
+            'title': 'Is this the right picture?',
+            'subtitle': 'Tap a button to answer.',
+            'image_url': attachmentUrl,
+            'buttons': [
+              {
+                'type': 'postback',
+                'title': 'Yes!',
+                'payload': 'yes',
+              },
+              {
+                'type': 'postback',
+                'title': 'No!',
+                'payload': 'no',
+              }
+            ],
+          }]
+        }
+      }
+    };
+  }
+
+  // Send the response message
+  callSendAPI(senderPsid, response);
+}
+
+// Handles messaging_postbacks events
+function handlePostback(senderPsid, receivedPostback) {
+  let response;
+
+  // Get the payload for the postback
+  let payload = receivedPostback.payload;
+
+  // Set the response based on the postback payload
+  if (payload === 'yes') {
+    response = { 'text': 'Thanks!' };
+  } else if (payload === 'no') {
+    response = { 'text': 'Oops, try sending another image.' };
+  }
+  // Send the message to acknowledge the postback
+  callSendAPI(senderPsid, response);
+}
+
+// Sends response messages via the Send API
+function callSendAPI(senderPsid, response) {
+
+  // The page access token we have generated in your app settings
+  const PAGE_ACCESS_TOKEN = facebookConfig.PAGE_ACCESS_TOKEN;
+
+  // Construct the message body
+  let requestBody = {
+    'recipient': {
+      'id': senderPsid
+    },
+    'message': response
+  };
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    'uri': 'https://graph.facebook.com/v2.6/me/messages',
+    'qs': { 'access_token': PAGE_ACCESS_TOKEN },
+    'method': 'POST',
+    'json': requestBody
+  }, (err, _res, _body) => {
+    if (!err) {
+      console.log('Message sent!');
+    } else {
+      console.error('Unable to send message:' + err);
+    }
+  });
+}
 
 // Adds support for GET requests to our webhook
 // This code adds support for the Messenger Platform's webhook verification to your webhook.
@@ -48,7 +153,7 @@ router.post('/', (req, res) => {
 router.get('/', (req, res) => {
 
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
+  let VERIFY_TOKEN = facebookConfig.VERIFY_TOKEN
 
   // Parse the query params
   let mode = req.query['hub.mode'];
