0388390a801d9be2279de3b1fddfef1ed2497501
DESKTOP-HDGTCHB\NhaNguyen <nguyenhoangvannha@gmail.com>
2021-05-19 18:26:03 +0700

    Fix import

--- models/course.model.js
+++ models/course.model.js
@@ -1,22 +1,22 @@
-import {course} from "../mock-data/mock-data"
+const { course } = require("../mock-data/mock-data")
 
 module.exports = {
     getAll() {
         return course;
     },
 
-    async getCourseByCateId(cateId){
+    async getCourseByCateId(cateId) {
         const listCourse = await course.filter(courseItem => courseItem.category_id == cateId);
         return listCourse;
     },
 
-    async getDetailCouresById(courseId){
-        const courseItem = await course.find(courseItem=> courseItem.course_id==courseId);
+    async getDetailCouresById(courseId) {
+        const courseItem = await course.find(courseItem => courseItem.course_id == courseId);
         return courseItem;
     },
 
-    async searchCourse(query){
-        const courseList  = await course.filter(courseItem=>courseItem.title.includes(query));
+    async searchCourse(query) {
+        const courseList = await course.filter(courseItem => courseItem.title.includes(query));
         return courseList;
     }
 
