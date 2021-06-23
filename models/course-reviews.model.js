a4f0b70ce66fd9a4c67792581fba7a2293b91914
DESKTOP-HDGTCHB\NhaNguyen <nguyenhoangvannha@gmail.com>
2021-06-23 10:20:51 +0700

    Add username when return reviews

--- models/course-reviews.model.js
+++ models/course-reviews.model.js
@@ -6,7 +6,10 @@ module.exports = {
         return db(TBL_COURSE_REVIEWS);
     },
     getReviewsByCourseId(courseId) {
-        return db(TBL_COURSE_REVIEWS).where('course_id', courseId);
+        return db(TBL_COURSE_REVIEWS)
+            .select([`${TBL_COURSE_REVIEWS}.*`, 'user.first_name', 'user.last_name'])
+            .where('course_id', courseId)
+            .innerJoin('user', `${TBL_COURSE_REVIEWS}.user_id`, 'user.user_id');
     },
     add(review) {
         return db(TBL_COURSE_REVIEWS).insert(review);
